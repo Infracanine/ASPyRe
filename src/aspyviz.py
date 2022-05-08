ce1e309752ca2bac0fbf33ed94dff2acd3c0af41
Samuel Taylor-Hansen <samueltaylorhansen@gmail.com>
2022-05-08 11:37:47 +0100

    refactoring to simplify things slightly, extract out model extraction and clingo parsing into seperate functions

--- src/aspyviz.py
+++ src/aspyviz.py
@@ -1,14 +1,11 @@
 from datetime import datetime
-import src.AtomSmasher.Atoms as Atoms
 from src.AtomSmasher.AtomImporter import AtomImporter
 import argparse
 import drawSvg as draw
 import os.path
 import re
-import inspect
 import sys
-from importlib import import_module
-import mylogging as mylogs
+from src.logging import mylogging as mylogs
 import time
 
 # Statics
@@ -25,13 +22,13 @@ logger = mylogs.MyLogger()
 
 
 # Stores a copy of the answer set input file alongside generated artefacts
-def write_answer_set_log(raw_answer_set: str, directory: str):
+def write_raw_clingo_to_file(raw_answer_set: str, directory: str):
     with open(f'{directory}/raw_pyviz_input.txt', 'w') as f:
         f.write(raw_answer_set)
 
 
 # Extract individual answers sets from raw text output of Clingo
-def extract_answer_sets(clingo_output: str) -> (str, list):
+def extract_models(clingo_output: str) -> (str, list):
     if "SATISFIABLE" not in clingo_output:
         raise Exception("Received non-satisfiable answer set.")
     # Extract answer sets from Clingo text output
@@ -42,10 +39,11 @@ def extract_answer_sets(clingo_output: str) -> (str, list):
 
 
 # Split an answer set string into substrings representing individual atoms
-def extract_atoms(answer_set):
-    return re.split("\s", answer_set.strip())
+def extract_atoms(model):
+    return re.split("\s", model.strip())
 
 
+# Calculate the approximate center of the artefact
 def calculate_centroid(render_points: list):
     xs = []
     ys = []
@@ -57,15 +55,14 @@ def calculate_centroid(render_points: list):
     return x_centroid, y_centroid
 
 
-# Do rendering in a better, object oriented way using a dictionary of objects mapping a regex to <something>
-def render_answer_set(atom_dict: dict, drawing: draw.Drawing, answer_set: str, title: str, scaling: int = 1, render_atom_text = False, ):
+def render_model(atom_dict: dict, drawing: draw.Drawing, answer_set: str, title: str, scaling: int = 1, render_atom_text = False, verbosity = False):
     # Draw background of image
     drawing.append(draw.Rectangle(0, 0, canvas_width, canvas_height, fill="white"))
     drawing.setPixelScale(PIXEL_SCALE)
     # Create objects list for pre-processing purposes
     points_set = set()
-
-    logger.log(f"Raw Clingo string to render: {answer_set}")
+    if verbosity:
+        logger.log(f"Raw Clingo string to render: {answer_set}")
     atoms = extract_atoms(answer_set)
     logger.log(f"Identified {len(atoms)} atoms, performing pre-processing.")
     atoms_and_orders = []
@@ -131,24 +128,74 @@ def generate_directory_name(title: str, root_dir: str):
             count += 1
             new_dir = f"{directory}[{count}]"
         directory = new_dir
-    return directory + "/"
+    return directory
 
 
 def pyviz_parser_factory():
-    my_parser = argparse.ArgumentParser(prog="PyViz", description="PyViz, a tool for visualising Answer Sets produced by CLINGO")
-    my_parser.add_argument("input_file", type=str, help="Specify the input file for PyViz, which should be a .txt")
-    my_parser.add_argument("output_dir", type=str, help="Specify the directory to output artefacts.")
-    my_parser.add_argument("mapping_file", type=str, help="Specify Python file containing atom mappings.")
-    my_parser.add_argument("-W", "--canvas_width", type=int, help="Specify the width of the canvases images generated")
-    my_parser.add_argument("-H", "--canvas_height", type=int, help="Specify the height of the canvases images generated")
-    my_parser.add_argument("-a", "--show_atom_text", action='store_true', help="Set if you wish to show the string representation of atoms rendered alongside each corresponding artefact.")
-    my_parser.add_argument("-t", "--title", type=str, help="Optional argument, specifying a title for the Answer Set being processed. This will be reflected in the name of the output folder created.")
-    my_parser.add_argument("-v", "--verbose", action='store_true', help="Configure verbosity of logging for PyViz")
+    my_parser = argparse.ArgumentParser(prog="PyViz", description="ASPyViz, a tool for visualising Answer Sets produced by CLINGO")
+    my_parser.add_argument("input_file", type=str,
+                           help="Specify the input file for PyViz, which should be a .txt")
+    my_parser.add_argument("output_dir", type=str,
+                           help="Specify the directory to output artefacts.")
+    my_parser.add_argument("mapping_file", type=str,
+                           help="Specify Python file containing atom mappings.")
+    my_parser.add_argument("-W", "--canvas_width",
+                           type=int,
+                           help="Specify the width of the canvases images generated")
+    my_parser.add_argument("-H", "--canvas_height",
+                           type=int,
+                           help="Specify the height of the canvases images generated")
+    my_parser.add_argument("-a", "--show_atom_text",
+                           action='store_true',
+                           help="Set if you wish to show the string representation of atoms rendered alongside each corresponding artefact.")
+    my_parser.add_argument("-t", "--title",
+                           type=str,
+                           help="Optional argument, specifying a title for the Answer Set being processed. "
+                                "This will be reflected in the name of the output folder created.")
+    my_parser.add_argument("-v", "--verbose",
+                           action='store_true',
+                           help="Configure verbosity of logging for ASPyViz.")
     return my_parser
 
 
+# Extract models from Clingo output
+def parse_clingo_output_file(input_file: str, output_dir: str):
+    with open(input_file) as f:
+        input_contents = f.read()
+    if input_contents == "":
+        logger.log("Input file was empty, exiting pyviz")
+        return
+    metadata, models = extract_models(input_contents)
+    print(output_dir)
+    logger.log(f"Extracted {len(models)} models from file {input_file}")
+    print("Contents:" + input_contents)
+    write_raw_clingo_to_file(raw_answer_set=input_contents, directory=output_dir)
+    return metadata, models
+
+
+# Takes a list of models, represented as strings, and renders them in the given directory
+def render_models(models: list,
+                  render_dir: str,
+                  atom_dict: dict,
+                  render_atom_text: bool,
+                  title=""):
+    model_count = 0
+    start_time = time.time()
+    for each in models:
+        model_count += 1
+        d = draw.Drawing(canvas_width, canvas_height, origin=(0, 0))
+        subtitle = (title + " ") if title != "" else ""
+
+        subtitle += f"Answer Set {model_count}"
+        render_model(drawing=d, answer_set=each, title=subtitle, atom_dict=atom_dict, scaling=RENDER_SCALING, render_atom_text=render_atom_text)
+        full_path = f"{render_dir}/AnswerSet{model_count}.png"
+        logger.log(f"Saved {subtitle} to '{full_path}'")
+        d.savePng(full_path)
+    execution_time = time.time() - start_time
+    logger.log(f"COMPLETE: Successfully rendered {model_count} answer sets in {float('%.5g' % execution_time)} seconds!")
+
+
 '''
-Main Pyviz script for visualising the output of Clingo
 Accept the text output of a Clingo execution, typically something like:
 
     clingo version 5.4.0
@@ -162,47 +209,11 @@ Accept the text output of a Clingo execution, typically something like:
     Calls        : 1
     Time         : 0.006s (Solving: 0.00s 1st Model: 0.00s Unsat: 0.00s)
     CPU Time     : 0.001s
-
-And outputs pngs based on user defined classes in the Atoms module
 '''
 
 
-def process_clingo_file(atom_dict: dict, input_file: str, output_dir: str, title: str, render_atom_text: bool):
-    # Open file
-    with open(input_file) as f:
-        input_contents = f.read()
-    if input_contents == "":
-        logger.log("Input file was empty, exiting pyviz")
-        return
-    # Validate input text file is correct format
-    metadata, answer_sets = extract_answer_sets(input_contents)
-    logger.log(f"Extracted {len(answer_sets)} answer sets\n")
-
-    # Calculate name of directory to store rendered artifacts in
-    directory = generate_directory_name(title, output_dir)
-    os.mkdir(directory)
-    write_answer_set_log(raw_answer_set=input_contents, directory=directory)
-
-    start_time = time.time()
-    # Iterate through all identified answer sets and render that output
-    answer_set_count = 0
-    for each in answer_sets:
-        answer_set_count += 1
-        d = draw.Drawing(canvas_width, canvas_height, origin=(0, 0))
-        subtitle = (title + " ") if title != "" else ""
-
-        subtitle += f"Answer Set {answer_set_count}"
-        render_answer_set(drawing=d, answer_set=each, title=subtitle, atom_dict=atom_dict, scaling=RENDER_SCALING, render_atom_text=render_atom_text)
-        full_path = f"{directory}AnswerSet{answer_set_count}.png"
-        logger.log(f"Saved {subtitle} to '{full_path}'")
-        d.savePng(full_path)
-
-    execution_time = time.time() - start_time
-    # Print time to 5 significant figures
-    logger.log(f"COMPLETE: Successfully rendered {answer_set_count} answer sets in {float('%.5g' % execution_time)} seconds!")
-
-
-# Main function, primarily concerned with argument parsing for the command line tool, and feeding these to our process clingo file
+# Main function, primarily concerned with argument parsing for the command line tool, and feeding these to our function
+# Which parses Clingo output into a set of models, and then renders the models
 def main(argv):
     # Parse arguments
     parser = pyviz_parser_factory()
@@ -222,12 +233,17 @@ def main(argv):
     except IOError as err:
         logger.log("IO error: {0}".format(err))
         return
-    atomimporter = AtomImporter(logger)
-    atom_dict = atomimporter.load_atoms(mapping_file)
-    process_clingo_file(atom_dict=atom_dict, input_file=input_file, output_dir=output_dir, title=raw_title, render_atom_text=render_atom_text,)
+    atom_importer = AtomImporter(logger)
+    atom_dict = atom_importer.load_atoms(mapping_file)
+    # Extract metadata and models from the clingo file
+    render_directory = generate_directory_name(raw_title, output_dir)
+    os.mkdir(render_directory)
+    metadata, models = parse_clingo_output_file(input_file, render_directory)
+    # Render models
+    render_models(models, output_dir, atom_dict, render_atom_text, raw_title)
 
-def validate_user_mappings_file():
-    first_line = "from src.AtomSmasher.Atoms import Atom"
+    # Print time to 5 significant figures
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
